b5fcbe568b8cd4d68ac99bcad59343043e1f1fe2
Mohammed Mohasin <mohasinctg@gmail.com>
2024-02-05 10:35:23 +0600

    Create main.js
    functions added

---
+++ main.js
@@ -0,0 +1,20 @@
+// Format dd/mm/yyyy
+console.log(removeLeadingZeroFromDate("New York, NY 07/08/2025"));
+console.log(removeLeadingZeroFromDate("New York, NY 07/11/2025"));
+console.log(removeLeadingZeroFromDate("New York, NY 17/08/2025"));
+console.log(removeLeadingZeroFromDate("New York, NY 07-08-2025"));
+console.log(removeLeadingZeroFromDate("New York, NY 07-10-2025"));
+console.log(removeLeadingZeroFromDate("New York, NY 17-08-2025"));
+
+/**
+ * remove leading zero from day and month
+ */
+function removeLeadingZeroFromDate(string){
+    let expected = string.match(/\d{1,2}\D\d{1,2}\D(\d{4}|\d{2})/g);
+    if (typeof expected == 'object' && expected != null) {
+        let date = Object.values(expected)[0];
+        let removeLeadingZero = Object.values(expected)[0].replace(/(^|-|\/)0+/g, "$1");
+        let result = string.replace(date, removeLeadingZero);
+        return result;
+    }else return string;
+}
